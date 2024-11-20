fb5191f508133c6df972d2d216d7525e1d0bd7e8
q1q1w2w2 <106463879+q1q1w2w2@users.noreply.github.com>
2024-11-20 18:30:50 +0900

    2024.11.20

--- codingTest/src/Main.java
+++ codingTest/src/Main.java
@@ -4,14 +4,8 @@ import java.util.stream.IntStream;
 public class Main {
 
     public static void main(String[] args) {
-        int[] a = {1, 2, 3, 4};
-        int[] b = {1, 2, 3, 4};
-        int[] c = new int[a.length];
-        IntStream.range(0, a.length).forEach(i -> {
-            c[i] = a[i] * b[i];
-        });
-        System.out.println("c = " + Arrays.toString(c));
-
-        System.out.println(IntStream.range(0, a.length).map(i -> a[i] * b[i]).sum());
+        int num = 100;
+        String string = Integer.toString(num, 3);
+        System.out.println("string = " + string);
     }
 }
